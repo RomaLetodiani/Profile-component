4035b94af250d1e397027b5da5a505c973dd0f99
RomaLetodiani <roman.letodiani03@gmail.com>
2023-09-23 18:28:32 +0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Stats from './Components/Stats';
 import './Components/style.scss';
+import avatar from './images/image-victor.jpg';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <div className="banner"></div>
         <div className="texts">
           <header>
-            <img src="/images/image-victor.jpg" alt="profile" />
+            <img src={avatar} alt="profile" />
             <h1>
               Victor Crest <span>26</span>
             </h1>
